6e2234f6ac0b68fd44a8872c143311270203ca2a
Azfar <hanbalflear@gmail.com>
2025-12-12 10:51:38 +0000

    Cleanup Notebook and update Dockerfile

--- mimic-project/db.py
+++ mimic-project/db.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import pandas as pd
 from sqlalchemy import create_engine, text
+import duckdb
 
 load_dotenv()
 
@@ -13,3 +14,21 @@ def run_query(sql):
 def temp_query(sql):
     with engine.connect() as conn:
         conn.execute(text(sql))
+
+def duck_query_df(query):
+    con = duckdb.connect()
+    con.install_extension('postgres')
+    con.load_extension('postgres')
+
+    # Clear cache to free up memory
+    con.sql("CALL pg_clear_cache();")
+
+    # Attach the Postgres database using environment variables
+    con.sql(f"ATTACH 'dbname={os.getenv('PGDATABASE')} user={os.getenv('PGUSER')} \
+            password={os.getenv('PGPASSWORD')} host={os.getenv('PGHOST')}'" \
+            "AS db (TYPE postgres, READ_ONLY);")
+    
+    # Run the query and return the result as a pandas DataFrame
+    result = con.sql(query).df()
+    con.close()
+    return result
